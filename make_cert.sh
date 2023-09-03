bcf1112d6657b16e85243ae4c1fbed4f327bbb6e
gengyong <gengyong@gmail.com>
2023-09-03 14:53:59 +0800

    fix cert error.

--- make_cert.sh
+++ make_cert.sh
@@ -9,8 +9,6 @@ if [ -z "$1" ]; then
     exit
 fi
 
-
-
 NGROK_DOMAIN=$1
 
 echo "make certificate for $NGROK_DOMAIN ..."
@@ -19,16 +17,16 @@ mkdir -p keys/$NGROK_DOMAIN
 cd keys/$NGROK_DOMAIN
 openssl genrsa -out rootCA.key 2048
 openssl req -x509 -new -nodes -key rootCA.key -out rootCA.pem \
-    -subj "/CN=$NGROK_DOMAIN" \
-    -days 5000
-openssl genrsa -out device.key 2048
-openssl req -new -key device.key -out device.csr \
-    -subj "/CN=$NGROK_DOMAIN" \
-    -addext "subjectAltName = DNS:$NGROK_DOMAIN"
-openssl x509 -req -in device.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out device.crt \
-    -days 5000
+    -days 5000 \
+    -subj "/CN=$NGROK_DOMAIN" 
+openssl genrsa -out server.key 2048
+openssl req -new -key server.key -out server.csr \
+    -subj "/CN=$NGROK_DOMAIN"
+openssl x509 -req -in server.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out server.crt \
+    -days 5000 \
+    -extfile <(printf "subjectAltName=DNS:$NGROK_DOMAIN")
 
 cp -f rootCA.pem ../../assets/client/tls/ngrokroot.crt
-cp -f device.crt ../../assets/server/tls/snakeoil.crt
-cp -f device.key ../../assets/server/tls/snakeoil.key
+cp -f server.crt ../../assets/server/tls/snakeoil.crt
+cp -f server.key ../../assets/server/tls/snakeoil.key
 cd -
